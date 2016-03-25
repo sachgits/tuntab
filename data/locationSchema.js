6a8b606807ffe197762687c5a0dbacd2ddcda0f9
sachgits <sachgits@gmail.com>
2016-03-25 22:37:06 +0300

    trying to unstash changes

--- data/locationSchema.js
+++ data/locationSchema.js
@@ -1,17 +1,15 @@
 /**
  * Created by sachg on 2/16/2016.
  */
-<<<<<<< Updated upstream
+
 const MONGO_CONNECTION = "mongodb://localhost/tuntab";
 const MONGO_USER = "admin";
 const MONGO_PASS = "12345";
 
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 if(!mongoose.connection)
     mongoose.connect(MONGO_CONNECTION);
-=======
-import {mongoose} from './dbConfig';
->>>>>>> Stashed changes
+
 
 var Schema = mongoose.Schema;
 
